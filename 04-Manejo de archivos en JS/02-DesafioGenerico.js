3789a9c47b9c9e279ba8370dd6e775bdeda004cf
Noelia Cid <noelia.cid.developer@gmail.com>
2022-03-24 18:57:57 -0300

    desafio entregable

--- 04-Manejo de archivos en JS/02-DesafioGenerico.js
+++ 04-Manejo de archivos en JS/02-DesafioGenerico.js
@@ -1,3 +1,12 @@
+/*
+    Realizar un programa que:
+A) Guarde en un archivo llamado fyh.txt la fecha y hora actual.
+B) Lea nuestro propio archivo de programa y lo muestre por consola.
+C) Incluya el manejo de errores con try catch (progresando las excepciones con throw new Error).
+
+Aclaración: utilizar las funciones sincrónicas de lectura y escritura de archivos del módulo fs de node.js
+*/
+
 const fs = require('fs');
 
 try{
---
+++ 04-Manejo de archivos en JS/03-DesafioEntregable.js
@@ -0,0 +1,101 @@
+/*
+     Manejo de archivos
+
+>> Consigna: Implementar programa que contenga una clase llamada Contenedor que reciba el nombre del archivo con el que va a trabajar e implemente los siguientes métodos:
+
+save(Object): Number - Recibe un objeto, lo guarda en el archivo, devuelve el id asignado.
+getById(Number): Object - Recibe un id y devuelve el objeto con ese id, o null si no está.
+getAll(): Object[] - Devuelve un array con los objetos presentes en el archivo.
+deleteById(Number): void - Elimina del archivo el objeto con el id buscado.
+deleteAll(): void - Elimina todos los objetos presentes en el archivo.
+---------------------------------------------------------------------------------------------------------------------------
+>> Aspectos a incluir en el entregable: 
+El método save incorporará al producto un id numérico, que deberá ser siempre uno más que el id del último objeto agregado (o id 1 si es el primer objeto que se agrega) y no puede estar repetido.
+Tomar en consideración el contenido previo del archivo, en caso de utilizar uno existente.
+Implementar el manejo de archivos con el módulo fs de node.js, utilizando promesas con async/await y manejo de errores.
+Probar el módulo creando un contenedor de productos, que se guarde en el archivo: “productos.txt”
+Incluir un llamado de prueba a cada método, y mostrando por pantalla según corresponda para verificar el correcto funcionamiento del módulo construído. 
+
+FORMATO DE LOS PRODUCTOS:                                                                                                                                          
+    {                                                                                                                                                    
+      title: 'Escuadra',                                                                                                                                 
+      price: 12.345,                                                                                                                                     
+      thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png',                                     
+      id: 1                                                                                                                                              
+    },                                                                                                                                                   
+    {                                                                                                                                                    
+      title: 'Calculadora',                                                                                                                              
+      price: 234.56,                                                                                                                                     
+      thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png',                                          
+      id: 2                                                                                                                                              
+    },                                                                                                                                                   
+    {                                                                                                                                                    
+      title: 'Globo Terráqueo',                                                                                                                          
+      price: 345.67,                                                                                                                                     
+      thumbnail: 'https://cdn3.iconfinder.com/data/icons/education-209/64/globe-earth-geograhy-planet-school-256.png',                                   
+      id: 3                                                                                                                                              
+    }                                                                                                                                                    
+  ]    
+
+*/
+const fs = require('fs');
+
+const filePath = '/productos.txt'
+const Productos = []
+
+class Contenedor {
+
+    constructor(title,price,thumbnail){
+        this.title = title,
+        this.price = price,
+        this.thumbnail = thumbnail    
+    }
+
+//METODO SAVE
+    save(product) {   
+        //SI NO HAY PRODUCTOS LE ASIGNAMOS EL ID 1, SI NO LE ASIGNAMOS 1 MAS QUE EL ÚLTIMO DEL ARRAY
+            if (Productos == []) {
+               product.id = 1
+            }
+            else{
+                product.id = Productos.length + 1
+            }
+        //LO AGREGAMOS AL ARRAY
+        Productos.push(product)
+
+        //LO AGREGAMOS AL ARCHIVO
+        // fs.writeFileSync(filePath, Productos) 
+        
+        //RETORNAMOS EL ID DEL PRODUCTO AGREGADO
+        return product.id
+    }
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//MÉTODO GET BY ID
+    getById (idNumber) {
+        try {
+            const data = Productos     /*JSON.parse(fs.readFileSync(filePath, 'utf-8'));*/
+            console.log(data);
+            const productId = data.find((element) => element.id == idNumber);
+
+                if (productId){
+
+                    console.log("ID A BUSCAR: ", idNumber, "PRODUCTO ENCONTRADO: ", productId);
+                }
+                else{
+                    return null
+                }
+          
+        } catch (err) {
+            console.log('Error lectura Sincronica');
+            console.log(err.message)
+          }
+    }
+} 
+
+///////////////////////////////////////////////////////////////////////////////
+
+const obj1 = new Contenedor ( 'Escuadra',200,'https://cdn3.iconfinder.com/data/icons/education-209/64/ruler-triangle-stationary-school-256.png')
+console.log(obj1);
+const guardarObjeto = obj1.save(obj1)
+console.log(guardarObjeto);
+
