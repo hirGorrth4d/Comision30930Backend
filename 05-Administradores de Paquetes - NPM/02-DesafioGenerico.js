7f18b59ea1192d6d73a7c128f3b7133c18e7e513
Noelia Cid <noelia.cid.developer@gmail.com>
2022-03-27 20:17:52 -0300

    desafio 2 clase 5

--- 05-Administradores de Paquetes - NPM/02-DesafioGenerico.js
+++ 05-Administradores de Paquetes - NPM/02-DesafioGenerico.js
@@ -1,3 +1,17 @@
+/*
+A- Crear un proyecto en node.js que genere 10000 números aleatorios en el rango  de 1 a 20.
+B- Crear un objeto cuyas claves sean los números salidos y el valor asociado a cada clave será la cantidad de veces que salió dicho número. Representar por consola los resultados.
+Y obtenga la siguiente información de dicho array
+A) Los nombres de los productos en un string separados por comas.
+B) El precio total
+C) El precio promedio
+D) El producto con menor precio
+E) El producto con mayor precio
+F) Con los datos de los puntos 1 al 5 crear un objeto y representarlo por consola
+
+*/
+
+
 const productos = [
     { id:1, nombre:'Escuadra', precio:323.45 },
     { id:2, nombre:'Calculadora', precio:234.56 },
@@ -8,23 +22,42 @@ const productos = [
 ]
 
 //EJERCICIO A
+//NOMBRES DE LOS PRODUCTOS
 const nombresProductos = productos.map((unProducto => unProducto.nombre))
 let ejercicioA = nombresProductos.join(',');
 
 //EJERCICIO B
+//PRECIO TOTAL
 let ejercicioB = 0;
 productos.forEach((unProducto) => {
     ejercicioB += unProducto.precio
 })
 
 //EJERCICIO C 
-
+//PRECIO PROMEDIO
 let ejercicioC = ejercicioB / productos.length;
 
-//EJERCICIO D
-
+//Ejercicio D y E => El producto con menor y mayor precio
+//ordenamos el array por precio y sabemos que el primero va a ser el menor valor y el ultimo el de mayor valor.
 const logicaDeComparacion = (a,b) => {
     if (a.precio > b.precio) return 1;
     else if (a.precio < b.precio) return -1;
     else 0;
 }
+
+const arrayOrdernado = productos.sort(logicaDeComparacion);
+
+const ejercicioD = arrayOrdernado[0];
+const ejercicioE = arrayOrdernado[arrayOrdernado.length - 1];
+
+//EJERCICIO F
+
+const salida = {
+    ejercicioA,
+    ejercicioB,
+    ejercicioC,
+    ejercicioD,
+    ejercicioE,
+  };
+
+  console.log(salida);
