42611c2fcc1a1031da00b916054775b4a0200997
Noelia Cid <noelia.cid.developer@gmail.com>
2022-03-27 20:08:21 -0300

    desafio clase 5

--- 05-Administradores de Paquetes - NPM/01-DesafioGenerico.js
+++ 05-Administradores de Paquetes - NPM/01-DesafioGenerico.js
@@ -1,4 +1,8 @@
+/*
+A- Crear un proyecto en node.js que genere 10000 números aleatorios en el rango  de 1 a 20.
+B- Crear un objeto cuyas claves sean los números salidos y el valor asociado a cada clave será la cantidad de veces que salió dicho número. Representar por consola los resultados.
 
+*/
 
 
 
@@ -14,7 +18,10 @@ const salida = {};
 for (let i = 0; i < 10000; i++) {
     const valor = between(limiteInferior,limiteSuperior);
 
-    if (salida[valor])
-    salida[valor]
-    
+    if(salida[valor])
+        salida[valor] = salida[valor]+1;
+    else
+        salida[valor] = 1;
 }
+
+console.log(salida);
