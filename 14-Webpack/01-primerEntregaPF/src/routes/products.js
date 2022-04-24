b8db5861702e4676f9b1de22387eeee32e1d49b9
Noelia Cid <noelia.cid.developer@gmail.com>
2022-04-24 19:30:12 -0300

    put funcionando en productos

--- 14-Webpack/01-primerEntregaPF/src/routes/products.js
+++ 14-Webpack/01-primerEntregaPF/src/routes/products.js
@@ -33,5 +33,38 @@ router.get('/:id', async(req,res) => {
   }
 })
 
+//post
+router.post('/', async(req,res) => {
+  const nuevoProducto = req.body
+  console.log('NUEVO PRODUCTO ===>>', nuevoProducto);
+  await ProductosController.save(nuevoProducto)
+  try {
+    const productos = await ProductosController.getAll();
+    res.render('pages/productos', {productos})
+  } catch (error) {
+    console.log('ERROR', error);
+  }
+})
+
+
+//put
+
+router.put('/:id', async (req,res) => {
+    
+const body = req.body;
+console.log(body);
+const id = req.params.id
+ try {
+  await ProductosController.Update(id,body);
+
+  const productos = await ProductosController.getAll();
+    res.render('pages/productos', {productos})
+ }
+
+ catch{
+   res.json({msg: "PRODUCTO NO ENCONTRADO"})
+ }
+})
+
 
 module.exports = router;
